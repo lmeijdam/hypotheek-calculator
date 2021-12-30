66ab74de6c3e4fea27f1cd6078ceb8ea25a19bb5
Lars Meijdam <lars.meijdam@teamrockstars.nl>
2021-12-30 13:50:58 +0100

    updated layout for extending and mobile support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Route, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Route[] = [
+{
+  path: '', component: HomeComponent, pathMatch: 'full'
+}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     {provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR'}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,57 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  calculatedValue!: number;
+  @ViewChild('hypotheekbedrag') hypotheekBedrag!: ElementRef<HTMLInputElement>;
+  @ViewChild('hypotheekrente') hypotheekRente!: ElementRef<HTMLInputElement>;
+  @ViewChild('hypotheektijd') hypotheekTijd!: ElementRef<HTMLInputElement>;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.calculate();
+    });
+
+  }
+
+  calculate() {
+    const looptijd = parseInt(this.hypotheekTijd.nativeElement.value);
+    const bedrag = parseInt(this.hypotheekBedrag.nativeElement.value);
+    const maandelijkseRente = parseFloat(this.hypotheekRente.nativeElement.value) / 100 /12;
+
+    this.calculatedValue = this.calculateInterest(maandelijkseRente, looptijd * 12, bedrag, 0);
+  }
+
+  private calculateInterest(ir: any, np: number, pv: number, fv: number, type = 0) {
+    /*
+     * ir   - interest rate per month
+     * np   - number of periods (months)
+     * pv   - present value
+     * fv   - future value
+     * type - when the payments are due:
+     *        0: end of the period, e.g. end of month (default)
+     *        1: beginning of period
+     */
+    var pmt, pvif;
+
+    fv || (fv = 0);
+    type || (type = 0);
+
+    if (ir === 0) return -(pv + fv) / np;
+
+    pvif = Math.pow(1 + ir, np);
+    pmt = (-ir * (pv * pvif + fv)) / (pvif - 1);
+
+    if (type === 1) pmt /= 1 + ir;
+
+    return -pmt;
+  }
+
+
+}
